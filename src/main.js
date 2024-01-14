136cf852f8e41b2f216bf52de13133b5426e7bdb
= <cjmoshy12@gmail.com>
2024-01-13 18:22:36 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// created by cj moshy 1/13/2024
+// cmpm 120
+
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480
+}
+
+let game = new Phaser.Game(config) //call phaser main class
+
